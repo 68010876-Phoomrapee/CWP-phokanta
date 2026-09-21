a272dec2fe31fcec3caeb99275153e1694959784
68010876-Phoomrapee <68010876@kmitl.ac.th>
2026-09-21 22:12:08 +0700

    cell02

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+def main():
+    number1 = int(input("Enter the first number : "))
+    number2 = int(input("Enter the second number : "))
+    result = number1 * number2
+    print(f"{number1} x {number2} = {result}")
+    if result == 0:
+        print("The result is positive and negative.")
+    elif result < 0:
+        print("The result is negative.")
+    else:
+        print("The result is positive.")
+main()
