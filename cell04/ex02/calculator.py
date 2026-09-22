049dd13038c136eede97149c1e28e36bc81cd8fd
68010876-Phoomrapee <68010876@kmitl.ac.th>
2026-09-22 14:37:33 +0700

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -4,5 +4,10 @@ num2 = int(input("Give me the second number: "))
 print("Thank you!")
 print(f"{num1} + {num2} = {num1 + num2}")
 print(f"{num1} - {num2} = {num1 - num2}")
-print(f"{num1} / {num2} = {num1 // num2}")
+
+if num2 == 0:
+    print(f"{num1} / {num2} = Undefined")
+else:
+    print(f"{num1} / {num2} = {num1 // num2}")
+
 print(f"{num1} * {num2} = {num1 * num2}")
