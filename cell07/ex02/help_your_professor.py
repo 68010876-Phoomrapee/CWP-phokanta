2023239c619600b830c4d97bd0f79bbec96e8568
68010876-Phoomrapee <68010876@kmitl.ac.th>
2026-09-24 00:36:29 +0700

    cell07

--- cell07/ex02/help_your_professor.py
+++ cell07/ex02/help_your_professor.py
@@ -6,6 +6,8 @@ class_3B = {
     "jean": 15,
     "coline": 8,
     "luc": 9
+    # 18 + 15 + 8 + 9 = 50
+    # 50 / 4 = 12.5
 }
 
 class_3C = {
@@ -13,6 +15,8 @@ class_3C = {
     "julie": 15,
     "marc": 8,
     "stephanie": 13
+    # 17 + 15 + 8 + 13 = 53
+    # 53 / 4 = 13.25
 }
 
 print(f"Average for class 3B: {average(class_3B)}.")
